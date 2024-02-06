fa0cf292a55e4992d495219626adbb461de125f0
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-06 16:09:40 +0500

    Jsx

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 class Greeting extends React.Component{
     render(){
-        return <h1>Hello {this.props.name}</h1>
+
+        const myElement= <div><h1>Hello {this.props.name}</h1></div>;
+        // React.createElement('h1',null,'hello greeting');
+        return myElement;
     }
 }
 
