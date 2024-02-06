1e0d9067e93a6743417f8f1dc11becf47d7577a6
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-06 18:16:00 +0500

    props array object

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import Welcome from './components/Welcome';
 import Greeting from './components/Greeting';
 import { FirstComponent as FC , SecondComponent as SC } from './components/MyComponent';
 import MC from './components/MyComponent';
+import Student from './components/Student';
 
 function App() {
+
+const student={
+  firstName:"Usman",
+  lastName:"Shabbir"
+}
+
+const skills=['HTML','CSS'];
+
   return (
     <div className="App">
-      <Greeting name="Shabbir" />
+      {/* <Greeting name="Shabbir" />
       <Welcome name="Usman"/>
       <h1>Hello World</h1>
       <FC />
       <SC />
-      <MC />
-      
+      <MC /> */}
+      {/* <Student firstName="Usman" lastName="Shabbir"/> */}
+      <Student data={skills}/>
     </div>
   );
 }
---
+++ src/components/Student.js
@@ -0,0 +1,22 @@
+// object 
+//const Student=(props) => {
+//     return(
+//         <div>
+//             <h1>student first name : {props.student.firstName}</h1>
+//             <h1>student last name : {props.student.lastName}</h1>
+//         </div>
+//     )
+// }
+
+//array
+const Student=(props) => {
+    return(
+        <div>
+            <h1>Skills : {props.data}</h1>
+        </div>
+    )
+}
+
+
+
+export default Student;
