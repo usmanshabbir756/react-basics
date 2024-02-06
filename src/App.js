4944004cc45344be42c3c756e2969f8abc8bd752
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-06 15:44:34 +0500

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Welcome from './components/Welcome';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
+      <Greeting name="Shabbir" />
       <Welcome name="Usman"/>
       <h1>Hello World</h1>
     </div>
